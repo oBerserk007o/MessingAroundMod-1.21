6a6e3a8f7de99ae87707db3d9beb6f24d3651887
st0x0ef <75510490+st0x0ef@users.noreply.github.com>
2024-09-02 09:12:43 -0400

    fix loading error

--- src/main/java/com/berserk007/messingaround/item/ModItems.java
+++ src/main/java/com/berserk007/messingaround/item/ModItems.java
@@ -2,24 +2,16 @@ package com.berserk007.messingaround.item;
 
 import com.berserk007.messingaround.MessingAroundMod;
 import com.berserk007.messingaround.item.custom.AdvancedClockItem;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.Item;
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class ModItems {
-    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MessingAroundMod.MOD_ID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(Registries.ITEM, MessingAroundMod.MOD_ID);
 
-
-    public static final DeferredItem<Item> METAL_COG =
-            ITEMS.registerItem("metal_cog", Item::new, new Item.Properties());
+    public static final DeferredHolder<Item, Item> METAL_COG = ITEMS.register("metal_cog", () -> new Item(new Item.Properties()));
 
     // To keep track of the time I'm wasting on fixing dumb bugs
-    public static final DeferredItem<Item> ADVANCED_CLOCK =
-            ITEMS.registerItem("advanced_clock", AdvancedClockItem::new, new Item.Properties());
-
-
-    public static void register(IEventBus eventBus) {
-        ITEMS.register(eventBus);
-    }
+    public static final DeferredHolder<Item, AdvancedClockItem> ADVANCED_CLOCK = ITEMS.register("advanced_clock", () -> new AdvancedClockItem(new Item.Properties()));
 }
